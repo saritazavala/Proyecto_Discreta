72eb27fa4f06b4b113ea93bf18df0a39ce22ef49
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-11-22 23:15:11 -0600

    Try added

--- Partition.py
+++ Partition.py
@@ -43,32 +43,38 @@ while True:
     print("2. Mensaje")
     print("3. Salir")
     print('')
-    opcion = input("Ingrese opcion deseada ")   
+    opcion = input("Ingrese opcion deseada ")
 
-#Basicamente, se pide el numero y la longitud de las particiones
+    try:
+        #Basicamente, se pide el numero y la longitud de las particiones
 #Luego se hace un ciclo for
 #Dentro del ciclo, para cada particion, se verifica si k (longitud) es 0 o no
 #Si no es igual a 0, la lungitud de la particion va a ser la longitud final
  
-    if opcion == "1":
-        number_parameter = input("Ingrese un número ")
-        particiones = create_partition(int(number_parameter))
-        k_longitud = input("Ingrese longitud a la que desea realizar la partición: ")
-        for i in particiones:
-            if int(k_longitud) == 0:
-                print(i)
-            elif len(i) == int(k_longitud):
-                print(i)
-	   
+        if opcion == "1":
+            number_parameter = input("Ingrese un número ")
+            particiones = create_partition(int(number_parameter))
+            k_longitud = input("Ingrese longitud a la que desea realizar la partición: ")
+            for i in particiones:
+                if int(k_longitud) == 0:
+                    print(i)
+                elif len(i) == int(k_longitud):
+                    print(i)
+               
 
-    #Extra para salvar el semestre
-    elif opcion == "2":
-        print("-----------------------------------")
-        print("Salvemos el semestre juntos")
-        print("-----------------------------------")
-    # Salir del programa        
-    elif opcion == "3" :
-            break
-    else:
-        print("Inngrese opcion válida")
-    
+        #Extra para salvar el semestre
+        elif opcion == "2":
+            print("-----------------------------------")
+            print("Salvemos el semestre juntos")
+            print("-----------------------------------")
+        # Salir del programa        
+        elif opcion == "3" :
+                break
+        else:
+            print("Inngrese opcion válida")
+        
+
+
+    except:
+        print("Hubo un mal ingreso")
+

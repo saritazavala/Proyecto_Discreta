fb5fbaf5d99bbb69504b2f07af3aa94a19dc7c09
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-11-22 22:34:44 -0600

    Almost finished x1000

--- Partition.py
+++ Partition.py
@@ -45,22 +45,30 @@ while True:
     print('')
     opcion = input("Ingrese opcion deseada ")   
 
+#Basicamente, se pide el numero y la longitud de las particiones
+#Luego se hace un ciclo for
+#Dentro del ciclo, para cada particion, se verifica si k (longitud) es 0 o no
+#Si no es igual a 0, la lungitud de la particion va a ser la longitud final
     try:
         if opcion == "1":
             number_parameter = input("Ingrese un número ")
             particiones = create_partition(int(number_parameter))
             k_longitud = input("Ingrese longitud a la que desea realizar la partición: ")
-            for elemnt in particiones:
-                if int(k_longitud) == 0:
-                    print(elemnt)
-                elif len(elemnt) == int(k_longitud):
-                    print(elemnt)
-    
-        
+            if k_longitud> number_parameter:
+                print("la longitud es mayor al numero que se quiere partir")
+            else: 
+                for elemnt in particiones:
+                    if int(k_longitud) == 0:
+                        print(elemnt)
+                    elif len(elemnt) == int(k_longitud):
+                        print(elemnt)
+                      
+    #Extra para salvar el semestre
         elif opcion == "2":
             print("-----------------------------------")
             print("Salvemos el semestre juntos")
             print("-----------------------------------")
+    # Salir del programa        
         elif opcion == "3" :
             break
         else:
